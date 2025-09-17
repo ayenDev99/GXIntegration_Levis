249cbe400d02fb69fa9d68c4ac56824c485e0c91
ayenDev99 <anchetakaren34@gmail.com>
2025-09-17 13:05:11 +0800

    Release to Supp
    Date: 09172025

--- GXIntegration/Views/OutboundEODTab.cs
+++ GXIntegration/Views/OutboundEODTab.cs
@@ -145,15 +145,15 @@ namespace GXIntegration_Levis.Views
 
 			try
 			{
-				//Logger.Log("[OUTBOUND - EOD] [TXT] Start Downloading files on local dir...");
-				//await OutboundPrice.Execute(repositories.PriceRepository, config);
-				//await OutboundInventorySnapshots.Execute(repositories.InventoryRepository, config);
-				//await OutboundInTransit.Execute(repositories.InTransitRepository, config);
-				//Logger.Log("[OUTBOUND - EOD] [TXT] Download process completed.");
+				Logger.Log("[OUTBOUND - EOD] [TXT] Start Downloading files on local dir...");
+				await OutboundPrice.Execute(repositories.PriceRepository, config);
+				await OutboundInventorySnapshots.Execute(repositories.InventoryRepository, config);
+				await OutboundInTransit.Execute(repositories.InTransitRepository, config);
+				Logger.Log("[OUTBOUND - EOD] [TXT] Download process completed.");
 
 				//Logger.Log("[OUTBOUND - EOD] [XML] Starting Downloading files on local dir...");
 				//await ExecuteStoreInventoryCountAsync();
-				await ExecuteAllAndSaveToSingleXmlAsync();
+				//await ExecuteAllAndSaveToSingleXmlAsync();
 				//Logger.Log("[OUTBOUND - EOD] [XML] Download process completed.");
 
 				//Logger.Log("[OUTBOUND - EOD] [SFTP] Start Uploading generated files to SFTP...");
@@ -203,35 +203,35 @@ namespace GXIntegration_Levis.Views
 					// Generate XML fragments
 					var xmlFragments = new[]
 					{
-						//OutboundStoreSale.GenerateXml(storeSaleItems, null, "template"),
-						//OutboundStoreShipping.GenerateXml(storeShippingItems, null, "template"),
-						//OutboundStoreReceiving.GenerateXml(storeReceivingItems, null, "template"),
-						//OutboundStoreInventoryAdjustment.GenerateXml(storeInventoryAdjustmentItems, null, "template"),
-						//OutboundStoreReturn.GenerateXml(storeReturnItems, null, "template"),
+						OutboundStoreSale.GenerateXml(storeSaleItems, null, "template"),
+						OutboundStoreShipping.GenerateXml(storeShippingItems, null, "template"),
+						OutboundStoreReceiving.GenerateXml(storeReceivingItems, null, "template"),
+						OutboundStoreInventoryAdjustment.GenerateXml(storeInventoryAdjustmentItems, null, "template"),
+						OutboundStoreReturn.GenerateXml(storeReturnItems, null, "template"),
 						OutboundStoreGoodsReturn.GenerateXml(storeGoodsReturnItems, null, "template"),
-						//OutboundStoreGoods.GenerateXml(storeGoodsItems, null, "template"),
+						OutboundStoreGoods.GenerateXml(storeGoodsItems, null, "template"),
 					};
 
 					string[] xmlTypes = new[]
 					{
-						//"StoreSale",
-						//"StoreShipping",
-						//"StoreReceiving",
-						//"StoreInventoryAdjustment",
-						//"StoreReturn",
+						"StoreSale",
+						"StoreShipping",
+						"StoreReceiving",
+						"StoreInventoryAdjustment",
+						"StoreReturn",
 						"StoreGoodsReturn",
-						//"StoreGoods",
+						"StoreGoods",
 					};
 
 					var dataModules = new List<(string Label, IEnumerable<object> Items)>
 					{
-						//("StoreSale", storeSaleItems as IEnumerable<object>),
-						//("StoreShipping", storeShippingItems as IEnumerable<object>),
-						//("StoreReceiving", storeReceivingItems as IEnumerable<object>),
-						//("StoreInventoryAdjustment", storeInventoryAdjustmentItems as IEnumerable<object>),
-						//("StoreReturn", storeReturnItems as IEnumerable<object>),
+						("StoreSale", storeSaleItems as IEnumerable<object>),
+						("StoreShipping", storeShippingItems as IEnumerable<object>),
+						("StoreReceiving", storeReceivingItems as IEnumerable<object>),
+						("StoreInventoryAdjustment", storeInventoryAdjustmentItems as IEnumerable<object>),
+						("StoreReturn", storeReturnItems as IEnumerable<object>),
 						("StoreGoodsReturn", storeGoodsReturnItems as IEnumerable<object>),
-						//("StoreGoods", storeGoodsItems as IEnumerable<object>),
+						("StoreGoods", storeGoodsItems as IEnumerable<object>),
 					};
 
 					//var storeRoot = new XElement("OutboundData");
